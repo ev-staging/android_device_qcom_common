LineageOS/android_hardware_qcom-caf_common

os_pickup_sepolicy_vndr: Include Lineage-specific QCOM sepolicy
Change-Id: Ibf70e4c8ab9d91b50c62c3e9f1263e1624e8ca00

os_pickup_sepolicy_vndr.mk
@@ -5,3 +5,5 @@ else ifneq ($(filter taro parrot,$(TARGET_BOARD_PLATFORM)),)
 else ifneq ($(filter kalama,$(TARGET_BOARD_PLATFORM)),)
     include device/qcom/sepolicy_vndr/sm8550/SEPolicy.mk
 endif
+
+-include device/lineage/sepolicy/qcom/sepolicy.mk

ev-staging/android_device_qcom_common

os_pickup_sepolicy_vndr.mk
@@ -7,3 +7,5 @@ else ifneq ($(filter $(UM_5_10_FAMILY),$(TARGET_BOARD_PLATFORM)),)
 else ifneq ($(filter $(UM_5_15_FAMILY),$(TARGET_BOARD_PLATFORM)),)
     include device/qcom/sepolicy_vndr/sm8550/SEPolicy.mk
 endif
+
+-include device/evervolv/sepolicy/qcom/sepolicy.mk